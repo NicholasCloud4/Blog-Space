3b2bf17d51625ff264290c5f0c2196b6aaf6e284
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-12 19:48:53 -0500

    Display blogs on page

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 /**
  Challenge:
+
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
  
- GET a list of blog posts from the JSON Placeholder API.
- 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
- 
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
- 
- Log the 5 items to the console
+ Hints: 
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you 
+   can then put into the div with `innerHTML`
  */
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "GET" })
     .then((response) => response.json())
     .then((data) => {
-        console.log(data.slice(0, 5))
+        const postArray = data.slice(0, 5);
+        let html = "";
+        for (let post of postArray) {
+            html += `<h1>${post.title}</h1>
+            <p>${post.body}</p>
+            <hr />`;
+        }
+        document.getElementById("blog-list").innerHTML = html;
     })
